0392673af75d7ca898f40017b86b1479165743ed
Demoferm <60359246+Demoferm@users.noreply.github.com>
2020-04-17 18:23:17 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,157 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+const columnsInitial = [
+    {
+        name: 'todo',
+        color: 'secondary',
+        tasks: [
+            {
+                id: 1,
+                name: 'todo 1'
+            },
+            {
+                id: 2,
+                name: 'todo 2'
+            },
+            {
+                id: 3,
+                name: 'todo 3'
+            }
+        ]
+    },
+
+    {
+        name: 'progress',
+        color: 'primary',
+        tasks: [
+            {
+                id: 4,
+                name: 'todo 4'
+            },
+            {
+                id:5,
+                name: 'todo 5'
+            },
+            {
+                id: 6,
+                name: 'todo 6'
+            }
+        ]
+    },
+
+    {
+        name:'review',
+        color: 'warning',
+        tasks: [
+            {
+                id: 7,
+                name: 'todo 7'
+            },
+            {
+                id: 8,
+                name: 'todo 8'
+            }
+        ]
+    },
+    {
+        name: 'done',
+        color: 'success',
+        tasks: [
+            {
+                id: 9,
+                name: 'todo 9'
+            },
+            {
+                id: 10,
+                name: 'todo 10'
+            }
+        ]
+    }
+
+]
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Board</h1>
 
-    </div>
-  );
+    const [columns, setColumns] = useState (columnsInitial)
+
+    const up = (arg) => {
+    console.log(arg);
+    const reranged = columns.map(col => {
+        if (col.name=== arg.columnName){
+            const tasks = col.tasks;
+            return {...col, tasks };
+        }
+        else return col;
+
+
+    })
+    setColumns(reranged);
+    }
+
+    const down = (arg) => {
+
+    }
+
+    return (
+        <div className="container">
+            <h1 className='mb-4 mt-4'>Board</h1>
+
+
+            <div className="row">
+                {
+                    columns.map(col => (
+                        <div className="col-sm d-flex">
+                            <div className={`border-top border-${col.color} border-width-4 w-100`}>
+                                <h3>{col.name}</h3>
+                                {
+                                    col.tasks.map(task => (
+                                    <div className="card text-center mb-2">
+                                        <div className="card-header">
+
+                                        </div>
+
+                                        <div className="card-body">
+                                            <div className="row">
+
+                                                <div className="col-3">
+                                                    <button onClick={ () => up({
+                                                        columnName: col.name,
+                                                        taskId: task.id
+
+                                                    }) }>Up</button>
+                                                    <button onClick={ () => down({
+                                                        columnName: col.name,
+                                                        taskId: task.id
+
+                                                    }) }>Down</button>
+                                                </div>
+
+                                                <div className="col-9">
+                                                    <h5 className="card-title">{task.name}</h5>
+
+                                                    <p className="card-text"></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                            <div className="card-footer text-muted">
+
+                                        </div>
+                                    </div>
+
+
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    ))
+                }
+
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
