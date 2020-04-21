97f8a835f2cc66d6b1d72a523edf9db6146f62d2
Demoferm <60359246+Demoferm@users.noreply.github.com>
2020-04-21 19:23:06 -0400

    add buttons "Left" & "Right"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const columnsInitial = [
     {
-        name: 'todo' ,
+        name: 'todo',
         color: 'secondary',
         tasks: [
             {
                 id: 1,
-                name: 'todo 11'
+                name: 'todo 1'
 
             },
             {
@@ -32,7 +32,7 @@ const columnsInitial = [
                 name: 'todo 4'
             },
             {
-                id:5,
+                id: 5,
                 name: 'todo 5'
             },
             {
@@ -43,7 +43,7 @@ const columnsInitial = [
     },
 
     {
-        name:'review',
+        name: 'review',
         color: 'warning',
         tasks: [
             {
@@ -75,24 +75,64 @@ const columnsInitial = [
 
 function App() {
 
-    const [columns, setColumns] = useState (columnsInitial)
+    const [columns, setColumns] = useState(columnsInitial)
 
-    const up = (arg) => {
-    console.log(arg);
-    const reranged = columns.map(col => {
-        if (col.name=== arg.columnName){
-            const tasks = col.tasks;
-            return {...col, tasks };
+    const moveToDown = (col,id) => {
+        if (id === col[col.length-1].id) return col;
+        let colCopy = [...col];
+        for (let i = 0; i < col.length-1; i++) {
+            if (col[i].id === id) {
+                colCopy[i + 1] = col[i];
+                colCopy[i] = col[i + 1];
+            }
         }
-        else return col;
-
-
-    })
-    setColumns(reranged);
+        return colCopy;
     }
 
-    const down = (arg) => {
+    const moveToUp = (col, id) => {
+        let colCopy = [...col];
+        if (id === col[0].id) return col;
+        for (let i = 0; i < col.length; i++) {
+            if (col[i].id === id) {
+                colCopy[i - 1] = col[i];
+                colCopy[i] = col[i - 1];
+            }
+        }
+        return colCopy;
+    }
 
+
+    const moveHorizontal = (arg) => {
+        const colIndex = columns.findIndex(col => col.name === arg.columnName);
+        if (arg.direction === 'left' && colIndex <=0) return;
+        if ((arg.direction === 'right' && colIndex === columns.length-1) || colIndex === -1) return;
+
+        const reranged = [...columns];
+        const taskIndex = columns[colIndex].tasks.findIndex(task => task.id === arg.taskId);
+        const task = reranged[colIndex].tasks[taskIndex];
+        reranged[colIndex].tasks = reranged[colIndex].tasks.filter( el => el.id != arg.taskId)
+        if (arg.direction==='left')
+            reranged[colIndex-1].tasks.push(task);
+        else
+            reranged[colIndex+1].tasks.push(task);
+
+        setColumns(reranged);
+    }
+
+
+    const moveVert = (arg) => {
+        console.log(arg);
+        const reranged = columns.map(col => {
+            if (col.name === arg.columnName) {
+                let tasks;
+                if (arg.direction === 'up')
+                    tasks = moveToUp(col.tasks, arg.taskId);
+                else
+                    tasks = moveToDown(col.tasks, arg.taskId);
+                return {...col, tasks};
+            } else return col;
+        })
+        setColumns(reranged);
     }
 
     return (
@@ -108,38 +148,61 @@ function App() {
                                 <h3>{col.name}</h3>
                                 {
                                     col.tasks.map(task => (
-                                    <div className="card text-center mb-2">
-                                        <div className="card-header">
+                                        <div className="card text-center mb-2">
+                                            <div className="card-header">
 
-                                        </div>
+                                            </div>
 
-                                        <div className="card-body">
-                                            <div className="row">
+                                            <div className="card-body">
+                                                <div className="row">
 
-                                                <div className="col-3">
-                                                    <button onClick={ () => up({
-                                                        columnName: col.name,
-                                                        taskId: task.id
+                                                    <div className="col-3">
+                                                        <button onClick={() => moveVert({
+                                                            columnName: col.name,
+                                                            taskId: task.id,
+                                                            direction: "up"
 
-                                                    }) }>&#11014;</button>
-                                                    <button onClick={ () => down({
-                                                        columnName: col.name,
-                                                        taskId: task.id
+                                                        })}>&#11014;</button>
+                                                        <button onClick={() => moveVert({
+                                                            columnName: col.name,
+                                                            taskId: task.id,
+                                                            direction: "down"
 
-                                                    }) }>&#11015;</button>
-                                                </div>
+                                                        })}>&#11015;</button>
+                                                    </div>
 
-                                                <div className="col-9">
-                                                    <h5 className="card-title">{task.name}</h5>
+                                                    <div className="col-9">
+                                                        <h5 className="card-title">{task.name}</h5>
 
-                                                    <p className="card-text"></p>
+                                                        <p className="card-text"></p>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
                                             <div className="card-footer text-muted">
+                                                    <div className="row">
+                                                        <div className="col-4">
+                                                            <button onClick={() => moveHorizontal( {
+                                                                    columnName: col.name,
+                                                                    taskId: task.id,
+                                                                    direction: "left"
+                                                                    }
+                                                                )
+                                                            }>&#11013;</button>
+                                                        </div>
+                                                        <div className="col-4">
+                                                        </div>
+                                                        <div className="col-4">
+                                                            <button onClick={ () => moveHorizontal({
+                                                                columnName: col.name,
+                                                                taskId: task.id,
+                                                                direction: "right"
+                                                                }
 
+                                                            )}>&#10145;</button>
+                                                        </div>
+                                                    </div>
+                                            </div>
                                         </div>
-                                    </div>
 
 
                                     ))
